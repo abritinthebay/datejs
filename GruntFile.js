23fe6ccc65e39870fd17e7700c7f321e14cee197
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2013-09-13 14:28:21 -0700

    removing old-i18n covert code

--- GruntFile.js
+++ GruntFile.js
@@ -107,119 +107,117 @@ module.exports = function(grunt) {
 			grunt.file.write(p, banner_compiled + sep + contents + sep + core );
 			grunt.log.writeln('File "' + p + '" created.');
 		});
-	});
-	grunt.registerMultiTask('convert', 'converts old DateJS format i18n into new format', function() {
-		var data = this.data,
-			path = require('path'),
-			dest = grunt.template.process(data.dest),
-			files = grunt.file.expand(data.src);
-		require('./'+data.core);
-		grunt.log.subhead('Loaded CultureStrings template file');
-		var base_data = Date.CultureInfo;
-		//     sep = grunt.util.linefeed,
-		//     banner_compiled = grunt.template.process(banner);
-		grunt.log.subhead('Processing i18n files...');
-		var banner = '/* \n' +
-			' * DateJS Culture String File\n' +
-			' * Country Code: <%= name %>\n' +
-			' * Name: <%= englishName %>\n' +
-			' * Format: "key" : "value"\n' +
-			' * Key is the en-US term, Value is the Key in the current language.\n' +
-			' */\n';
-		files.forEach(function(f) {
-			require('./'+f);
-			grunt.log.ok('Loaded: ' + path.basename(f).replace('.js', ''));
-			if (path.basename(f).replace('.js', '') === 'en-US') {
-				Date.CultureInfo = base_data;
-			}
-			var i, key, pattern,
-				name = Date.CultureInfo.name,
-				englishName = Date.CultureInfo.englishName,
-				output = {};
-			grunt.log.ok(name);
-			var banner_compiled = grunt.template.process(banner, {data:{name: name, englishName: englishName}});
-			for (var item in Date.CultureInfo) {
-				if (Date.CultureInfo.hasOwnProperty(item)) {
-					// grunt.log.writeln(Date.CultureInfo[item]);
-					if (item === 'name' || item === 'englishName' || item === 'nativeName') {
-						grunt.log.writeln(Date.CultureInfo[item]);
-						output[item] = Date.CultureInfo[item];
-					} else {
-						switch(item){
-							case 'dayNames':
-							case 'abbreviatedDayNames':
-							case 'shortestDayNames':
-							case 'monthNames':
-								for (i=0;i<Date.CultureInfo[item].length;i++) {
-									// grunt.log.writeln(base_data[item][i]);
-									output[base_data[item][i]] = Date.CultureInfo[item][i];
-								}
-								break;
-							case 'firstLetterDayNames':
-							case 'abbreviatedMonthNames':
-								var suffix = '_Abbr';
-								if (item === 'firstLetterDayNames') {
-									suffix = '_Initial';
-								}
-								for (i=0;i<Date.CultureInfo[item].length;i++) {
-									if (item === 'firstLetterDayNames') {
-										key = base_data[item][i]+'_'+base_data.abbreviatedDayNames[i]+suffix;
-									} else {
-										key = base_data.abbreviatedMonthNames[i]+suffix;
-									}
-									output[key] = Date.CultureInfo[item][i];
-								}
-								break;
-							case 'firstDayOfWeek':
-							case 'twoDigitYearMax':
-								output[item] = Date.CultureInfo[item];
-								break;
-							case 'formatPatterns':
-								for (pattern in Date.CultureInfo[item]) {
-									if (Date.CultureInfo[item].hasOwnProperty(pattern)) {
-										output[base_data[item][pattern]] = Date.CultureInfo[item][pattern];
-									}
-								}
-								break;
-							case 'regexPatterns':
-								for (pattern in Date.CultureInfo[item]) {
-									if (Date.CultureInfo[item].hasOwnProperty(pattern)) {
-										output[base_data[item][pattern].source] = Date.CultureInfo[item][pattern].source;
-									}
-								}
-								break;
-							case 'timezones':
-								for (var zone in base_data[item]) {
-									if (base_data[item].hasOwnProperty(zone)) {
-										output[zone] = base_data[item][zone];
-									}
-								}
-								break;
-							default:
-								if (typeof Date.CultureInfo[item] === 'string') {
-									output[base_data[item]] = Date.CultureInfo[item];
-								}
-								break;
-						}
-						
-					}
-				}
-			}
-
-			// grunt.log.writeln(banner_compiled);
-			// grunt.log.writeln(JSON.stringify(output, null, '	'));
-		    var p = dest + '/' + path.basename(f);
-		    grunt.file.write(p, banner_compiled + 'Date.CultureStrings = ' + JSON.stringify(output, null, '	') + ';\n');
-		    grunt.log.writeln('File "' + p + '" converted.');
-		});
+		grunt.file.delete(dirs.build+'/date-core.js');
 	});
 	grunt.registerTask('build_dev', ['concat:core', 'concat:basic', 'i18n:core']);
 	grunt.registerTask('build_prod', ['concat:core', 'concat:basic', 'i18n:core', 'uglify:production']);
-	grunt.registerTask('testit', ['convert:core']);
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	// Default task(s).
-	// grunt.registerTask('default', ['uglify']);
+	// grunt.registerMultiTask('convert', 'converts old DateJS format i18n into new format', function() {
+	// 	var data = this.data,
+	// 		path = require('path'),
+	// 		dest = grunt.template.process(data.dest),
+	// 		files = grunt.file.expand(data.src);
+	// 	require('./'+data.core);
+	// 	grunt.log.subhead('Loaded CultureStrings template file');
+	// 	var base_data = Date.CultureInfo;
+	// 	//     sep = grunt.util.linefeed,
+	// 	//     banner_compiled = grunt.template.process(banner);
+	// 	grunt.log.subhead('Processing i18n files...');
+	// 	var banner = '/* \n' +
+	// 		' * DateJS Culture String File\n' +
+	// 		' * Country Code: <%= name %>\n' +
+	// 		' * Name: <%= englishName %>\n' +
+	// 		' * Format: "key" : "value"\n' +
+	// 		' * Key is the en-US term, Value is the Key in the current language.\n' +
+	// 		' */\n';
+	// 	files.forEach(function(f) {
+	// 		require('./'+f);
+	// 		grunt.log.ok('Loaded: ' + path.basename(f).replace('.js', ''));
+	// 		if (path.basename(f).replace('.js', '') === 'en-US') {
+	// 			Date.CultureInfo = base_data;
+	// 		}
+	// 		var i, key, pattern,
+	// 			name = Date.CultureInfo.name,
+	// 			englishName = Date.CultureInfo.englishName,
+	// 			output = {};
+	// 		grunt.log.ok(name);
+	// 		var banner_compiled = grunt.template.process(banner, {data:{name: name, englishName: englishName}});
+	// 		for (var item in Date.CultureInfo) {
+	// 			if (Date.CultureInfo.hasOwnProperty(item)) {
+	// 				// grunt.log.writeln(Date.CultureInfo[item]);
+	// 				if (item === 'name' || item === 'englishName' || item === 'nativeName') {
+	// 					grunt.log.writeln(Date.CultureInfo[item]);
+	// 					output[item] = Date.CultureInfo[item];
+	// 				} else {
+	// 					switch(item){
+	// 						case 'dayNames':
+	// 						case 'abbreviatedDayNames':
+	// 						case 'shortestDayNames':
+	// 						case 'monthNames':
+	// 							for (i=0;i<Date.CultureInfo[item].length;i++) {
+	// 								// grunt.log.writeln(base_data[item][i]);
+	// 								output[base_data[item][i]] = Date.CultureInfo[item][i];
+	// 							}
+	// 							break;
+	// 						case 'firstLetterDayNames':
+	// 						case 'abbreviatedMonthNames':
+	// 							var suffix = '_Abbr';
+	// 							if (item === 'firstLetterDayNames') {
+	// 								suffix = '_Initial';
+	// 							}
+	// 							for (i=0;i<Date.CultureInfo[item].length;i++) {
+	// 								if (item === 'firstLetterDayNames') {
+	// 									key = base_data[item][i]+'_'+base_data.abbreviatedDayNames[i]+suffix;
+	// 								} else {
+	// 									key = base_data.abbreviatedMonthNames[i]+suffix;
+	// 								}
+	// 								output[key] = Date.CultureInfo[item][i];
+	// 							}
+	// 							break;
+	// 						case 'firstDayOfWeek':
+	// 						case 'twoDigitYearMax':
+	// 							output[item] = Date.CultureInfo[item];
+	// 							break;
+	// 						case 'formatPatterns':
+	// 							for (pattern in Date.CultureInfo[item]) {
+	// 								if (Date.CultureInfo[item].hasOwnProperty(pattern)) {
+	// 									output[base_data[item][pattern]] = Date.CultureInfo[item][pattern];
+	// 								}
+	// 							}
+	// 							break;
+	// 						case 'regexPatterns':
+	// 							for (pattern in Date.CultureInfo[item]) {
+	// 								if (Date.CultureInfo[item].hasOwnProperty(pattern)) {
+	// 									output[base_data[item][pattern].source] = Date.CultureInfo[item][pattern].source;
+	// 								}
+	// 							}
+	// 							break;
+	// 						case 'timezones':
+	// 							for (var zone in base_data[item]) {
+	// 								if (base_data[item].hasOwnProperty(zone)) {
+	// 									output[zone] = base_data[item][zone];
+	// 								}
+	// 							}
+	// 							break;
+	// 						default:
+	// 							if (typeof Date.CultureInfo[item] === 'string') {
+	// 								output[base_data[item]] = Date.CultureInfo[item];
+	// 							}
+	// 							break;
+	// 					}
+						
+	// 				}
+	// 			}
+	// 		}
+
+	// 		// grunt.log.writeln(banner_compiled);
+	// 		// grunt.log.writeln(JSON.stringify(output, null, '	'));
+	// 	    var p = dest + '/' + path.basename(f);
+	// 	    grunt.file.write(p, banner_compiled + 'Date.CultureStrings = ' + JSON.stringify(output, null, '	') + ';\n');
+	// 	    grunt.log.writeln('File "' + p + '" converted.');
+	// 	});
+	// });
 };
