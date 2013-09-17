537ea14f6aad1fc2443e2336b64a4ba4aeab49ff
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2013-09-17 12:49:43 -0700

    fixed #85 - completely replaced overly complex and broken getWeek and getISOWeek implementations.

--- core/core.js
+++ core/core.js
@@ -341,44 +341,34 @@
 	 * The .getWeek() function does NOT convert the date to UTC. The local datetime is used. Please use .getISOWeek() to get the week of the UTC converted date.
 	 * @return {Number}  1 to 53
 	 */
-	$P.getWeek = function () {
-		var a, b, c, d, e, f, g, n, s, w;
-		
-		$y = (!$y) ? this.getFullYear() : $y;
-		$m = (!$m) ? this.getMonth() + 1 : $m;
-		$d = (!$d) ? this.getDate() : $d;
-
-		if ($m <= 2) {
-			a = $y - 1;
-			b = (a / 4 | 0) - (a / 100 | 0) + (a / 400 | 0);
-			c = ((a - 1) / 4 | 0) - ((a - 1) / 100 | 0) + ((a - 1) / 400 | 0);
-			s = b - c;
-			e = 0;
-			f = $d - 1 + (31 * ($m - 1));
+	$P.getWeek = function (utc) {
+		// Create a copy of this date object  
+		var self, target = new Date(this.valueOf());
+		if (utc) {
+			target.addMinutes(target.getTimezoneOffset());
+			self = target.clone();
 		} else {
-			a = $y;
-			b = (a / 4 | 0) - (a / 100 | 0) + (a / 400 | 0);
-			c = ((a - 1) / 4 | 0) - ((a - 1) / 100 | 0) + ((a - 1) / 400 | 0);
-			s = b - c;
-			e = s + 1;
-			f = $d + ((153 * ($m - 3) + 2) / 5) + 58 + s;
+			self = this;
 		}
-		
-		g = (a + b) % 7;
-		d = (f + g - e) % 7;
-		n = (f + 3 - d) | 0;
-
-		if (n < 0) {
-			w = 53 - ((g - s) / 5 | 0);
-		} else if (n > 364 + s) {
-			w = 1;
-		} else {
-			w = (n / 7 | 0) + 1;
+		// ISO week date weeks start on monday  
+		// so correct the day number  
+		var dayNr = (self.getDay() + 6) % 7;
+		// ISO 8601 states that week 1 is the week  
+		// with the first thursday of that year.  
+		// Set the target date to the thursday in the target week  
+		target.setDate(target.getDate() - dayNr + 3);
+		// Store the millisecond value of the target date  
+		var firstThursday = target.valueOf();
+		// Set the target to the first thursday of the year  
+		// First set the target to january first  
+		target.setMonth(0, 1);
+		// Not a thursday? Correct the date to the next thursday  
+		if (target.getDay() !== 4) {
+			target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
 		}
-		
-		$y = $m = $d = null;
-		
-		return w;
+		// The weeknumber is the number of weeks between the   
+		// first thursday of the year and the thursday in the target week  
+		return 1 + Math.ceil((firstThursday - target) / 604800000); // 604800000 = 7 * 24 * 3600 * 1000  
 	};
 	
 	/**
@@ -387,10 +377,7 @@
 	 * @return {String}  "01" to "53"
 	 */
 	$P.getISOWeek = function () {
-		$y = this.getUTCFullYear();
-		$m = this.getUTCMonth() + 1;
-		$d = this.getUTCDate();
-		return p(this.getWeek());
+		return p(this.getWeek(true));
 	};
 
 	/**
