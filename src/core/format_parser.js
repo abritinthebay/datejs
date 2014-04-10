1d37041a9b6ddce4dff1e5cd440c67574dda0990
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2014-04-09 23:33:35 -0700

    minor refactoring and reorganization

--- src/core/format_parser.js
+++ src/core/format_parser.js
@@ -11,99 +11,73 @@
 		leap: [0,31,60,91,121,152,182,213,244,274,305,335]
 	};
 
-	var multiReplace = function (str, hash ) {
-		var key;
-		for (key in hash) {
-			if (Object.prototype.hasOwnProperty.call(hash, key)) {
-				var regex = (hash[key] instanceof RegExp) ? hash[key] : new RegExp(hash[key], "g");
-				str = str.replace(regex, key);
-			}
-		}
-		return str;
-	};
-
 	$P.isLeapYear = function(year) {
 		return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 	};
 
-	var getDayOfYearFromWeek = function (obj) {
-		var d, jan4, offset;
-		obj.weekDay = (!obj.weekDay && obj.weekDay !== 0) ? 1 : obj.weekDay;
-		d = new Date(obj.year, 0, 4);
-		jan4 = d.getDay() === 0 ? 7 : d.getDay(); // JS is 0 indexed on Sunday.
-		offset = jan4+3;
-		obj.dayOfYear = ((obj.week * 7) + (obj.weekDay === 0 ? 7 : obj.weekDay))-offset;
-		return obj;
-	};
-	var getDayOfYear = function (obj, dayOffset) {
-		if (!obj.dayOfYear) {
-			obj = getDayOfYearFromWeek(obj);
-		}
-		for (var i=0;i <= dayOffset.length;i++) {
-			if (obj.dayOfYear < dayOffset[i] || i === dayOffset.length) {
-				obj.day = obj.day ? obj.day : (obj.dayOfYear - dayOffset[i-1]);
-				break;
+	var utils = {
+		multiReplace : function (str, hash ) {
+			var key;
+			for (key in hash) {
+				if (Object.prototype.hasOwnProperty.call(hash, key)) {
+					var regex = (hash[key] instanceof RegExp) ? hash[key] : new RegExp(hash[key], "g");
+					str = str.replace(regex, key);
+				}
+			}
+			return str;
+		},
+		getDayOfYearFromWeek : function (obj) {
+			var d, jan4, offset;
+			obj.weekDay = (!obj.weekDay && obj.weekDay !== 0) ? 1 : obj.weekDay;
+			d = new Date(obj.year, 0, 4);
+			jan4 = d.getDay() === 0 ? 7 : d.getDay(); // JS is 0 indexed on Sunday.
+			offset = jan4+3;
+			obj.dayOfYear = ((obj.week * 7) + (obj.weekDay === 0 ? 7 : obj.weekDay))-offset;
+			return obj;
+		},
+		getDayOfYear : function (obj, dayOffset) {
+			if (!obj.dayOfYear) {
+				obj = utils.getDayOfYearFromWeek(obj);
+			}
+			for (var i=0;i <= dayOffset.length;i++) {
+				if (obj.dayOfYear < dayOffset[i] || i === dayOffset.length) {
+					obj.day = obj.day ? obj.day : (obj.dayOfYear - dayOffset[i-1]);
+					break;
+				} else {
+					obj.month = i;
+				}
+			}
+			return obj;
+		},
+		adjustForTimeZone : function (obj, date) {
+			var offset;
+			if (obj.zone.toUpperCase() === "Z" || (obj.zone_hours === 0 && obj.zone_minutes === 0)) {
+				// it's UTC/GML so work out the current timeszone offset
+				offset = -date.getTimezoneOffset();
 			} else {
-				obj.month = i;
+				offset = (obj.zone_hours*60) + (obj.zone_minutes || 0);
+				if (obj.zone_sign === "+") {
+					offset *= -1;
+				}
+				offset -= date.getTimezoneOffset();
 			}
-		}
-		return obj;
-	};
-	var adjustForTimeZone = function (obj, date) {
-		var offset;
-		if (obj.zone.toUpperCase() === "Z" || (obj.zone_hours === 0 && obj.zone_minutes === 0)) {
-			// it's UTC/GML so work out the current timeszone offset
-			offset = -date.getTimezoneOffset();
-		} else {
-			offset = (obj.zone_hours*60) + (obj.zone_minutes || 0);
-			if (obj.zone_sign === "+") {
-				offset *= -1;
+			date.setMinutes(date.getMinutes()+offset);
+			return date;
+		},
+		setDefaults : function (obj) {
+			obj.year = obj.year || Date.today().getFullYear();
+			obj.hours = obj.hours || 0;
+			obj.minutes = obj.minutes || 0;
+			obj.seconds = obj.seconds || 0;
+			obj.milliseconds = obj.milliseconds || 0;
+			if (!(!obj.month && (obj.week || obj.dayOfYear))) {
+				// if we have a month, or if we don't but don't have the day calculation data
+				obj.month = obj.month || 0;
+				obj.day = obj.day || 1;
 			}
-			offset -= date.getTimezoneOffset();
-		}
-		date.setMinutes(date.getMinutes()+offset);
-		return date;
-	};
-	var setDefaults = function (obj) {
-		obj.year = obj.year || Date.today().getFullYear();
-		obj.hours = obj.hours || 0;
-		obj.minutes = obj.minutes || 0;
-		obj.seconds = obj.seconds || 0;
-		obj.milliseconds = obj.milliseconds || 0;
-		if (!(!obj.month && (obj.week || obj.dayOfYear))) {
-			// if we have a month, or if we don't but don't have the day calculation data
-			obj.month = obj.month || 0;
-			obj.day = obj.day || 1;
-		}
-		return obj;
-	};
-	$P.processTimeObject = function (obj) {
-		var date, dayOffset;
-
-		setDefaults(obj);
-		dayOffset = ($P.isLeapYear(obj.year)) ? dayOffsets.leap : dayOffsets.standard;
-
-		if (!obj.month && (obj.week || obj.dayOfYear)) {
-			getDayOfYear(obj, dayOffset);
-		} else {
-			obj.dayOfYear = dayOffset[obj.month] + obj.day;
-		}
-
-		date = new Date(obj.year, obj.month, obj.day, obj.hours, obj.minutes, obj.seconds, obj.milliseconds);
-
-		if (obj.zone) {
-			adjustForTimeZone(obj, date); // adjust (and calculate) for timezone
-		}
-		return date;
-	};
-	
-	$P.ISO = {
-		regex : /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-3])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-4])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?\s?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/,
-		parse : function (s) {
-			var data = s.match(this.regex);
-			if (!data || !data.length) {
-				return null;
-			}
+			return obj;
+		},
+		buildTimeObjectFromData: function (data) {
 			var time = {
 				year : data[1] ? Number(data[1]) : data[1],
 				month : data[5] ? (Number(data[5])-1) : data[5],
@@ -128,12 +102,47 @@
 					time.seconds = data[18];
 				}
 			}
+			return time;
+		}
+	};
+
+	$P.processTimeObject = function (obj) {
+		var date, dayOffset;
+
+		utils.setDefaults(obj);
+		dayOffset = ($P.isLeapYear(obj.year)) ? dayOffsets.leap : dayOffsets.standard;
+
+		if (!obj.month && (obj.week || obj.dayOfYear)) {
+			utils.getDayOfYear(obj, dayOffset);
+		} else {
+			obj.dayOfYear = dayOffset[obj.month] + obj.day;
+		}
+
+		date = new Date(obj.year, obj.month, obj.day, obj.hours, obj.minutes, obj.seconds, obj.milliseconds);
+
+		if (obj.zone) {
+			utils.adjustForTimeZone(obj, date); // adjust (and calculate) for timezone
+		}
+		return date;
+	};
+	
+	$P.ISO = {
+		regex : /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-3])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-4])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?\s?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/,
+		parse : function (s) {
+			var time, data = s.match(this.regex);
+			if (!data || !data.length) {
+				return null;
+			}
+
+			time = buildTimeObjectFromData(data);
+
 			if (!time.year || (!time.year && (!time.month && !time.day) && (!time.week && !time.dayOfYear)) ) {
 				return null;
 			}
 			return $P.processTimeObject(time);
 		}
 	};
+
 	$P.Numeric = {
 		isNumeric: function (e){return!isNaN(parseFloat(e))&&isFinite(e);},
 		regex: /\b([0-1]?[0-9])([0-3]?[0-9])([0-2]?[0-9]?[0-9][0-9])\b/i,
@@ -169,6 +178,7 @@
 			return $P.processTimeObject(time);
 		}
 	};
+
 	$P.Normalizer = {
 		combineRegex: function (r1, r2) {
 			return new RegExp("(("+r1.source+")\\s("+r2.source+"))");
@@ -233,7 +243,7 @@
 			s =	s.replace($R.amThisMorning, function(str, am){return am;})
 				.replace($R.amThisEvening, function(str, pm){return pm;});
 
-			s = multiReplace(s, replaceHash);
+			s = utils.multiReplace(s, replaceHash);
 
 			try {
 				var n = s.split(/([\s\-\.\,\/\x27]+)/);
